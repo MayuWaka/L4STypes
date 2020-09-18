c596d25866a3ac03ae06ea59eeffe003d645a6a9
MayuWaka <180781056@ccmailg.meijo-u.ac.jp>
2020-09-19 00:44:13 +0900

    スキルリスト型(9/18)

---
+++ app/src/main/java/app/wakayama/tama/l4stypes/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.wakayama.tama.l4stypes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
